3b054b1fc2c7d22a37ae4979e5378c692da87d33
Dantas11 <thiagoo.dantas11@gmail.com>
2023-06-03 21:42:29 +1000

    conditional statements added

--- Assets/javascript/script.js
+++ Assets/javascript/script.js
@@ -6,16 +6,46 @@ const upperCaseLetters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N
 const specialCharacters = ['!','@','#','$','%','^','&','*','(',')','-','_','+','=','[',']','{','}','|',"\\",':',';','"',"'",'<','>',',','.','/','?',];
 const numericCharacters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
-console.log(lowerCaseLetters, upperCaseLetters, specialCharacters, numericCharacters);
+
 
 // Generate password
 function generatePassword() {
   var passwordChoice = prompt("How many characters would you like your password to contain?");
+
+if ((passwordChoice <8) || (passwordChoice >128)){
+alert("choose a length of at least 8 characters and no more than 128 characters");
+writePassword();
+}
+
   var confirmSpecialCharacters = confirm("Click ok to confirm including special characters.");
-  var confirmNumericCharacters = confirm("Click ok to confirm including numeric characters")
+  var confirmNumericCharacters = confirm("Click ok to confirm including numeric characters");
   var confirmLowerCaseCharacters = confirm("Click ok to confirm including lowercase characters");
   var confirmUpperCaseCharacters = confirm("Click ok to confirm including uppercase characters");
-  console.log(passwordChoice, specialCharacters, numericCharacters, lowerCaseCharacters, upperCaseCharacters);
+  
+
+  var arrayList = [];
+  
+  if(confirmSpecialCharacters === true){
+arrayList = arrayList.concat(specialCharacters);
+}
+if(confirmNumericCharacters === true){
+  arrayList = arrayList.concat(numericCharacters);
+}
+if(confirmLowerCaseCharacters === true){
+  arrayList = arrayList.concat(lowerCaseLetters);
+} 
+if(confirmUpperCaseCharacters === true){
+  arrayList = arrayList.concat(upperCaseLetters);
+}
+if ((confirmSpecialCharacters === false) && (confirmNumericCharacters === false) && (confirmLowerCaseCharacters === false) && (confirmUpperCaseCharacters === false)){
+  alert("Choose at least one condition");
+  writePassword();
+}
+
+console.log(arrayList);
+
+test = Math.random() = arrayList.length
+
 }
 
 // Write password to the #password input
